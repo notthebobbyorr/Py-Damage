13061bbcd5c708d2604fb1bcbaba824031247e1c
Robert Orr <44117437+notthebobbyorr@users.noreply.github.com>
2026-05-15 13:59:45 -0400

    Change default minimum value to 25

--- app/pages/hitters.py
+++ app/pages/hitters.py
@@ -103,7 +103,7 @@ def hitter_individual_stats():
                 "Minimum Value",
                 min_value=0,
                 max_value=500,
-                value=1,
+                value=25,
                 step=1,
                 key="hitter_stats_min_value",
             )
